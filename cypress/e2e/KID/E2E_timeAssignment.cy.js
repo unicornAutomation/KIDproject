5f6b7951de44cc35e9beed1d5a513e10bca0e034
unicornAutomation <solution.unicorn@outlook.co.th>
2023-03-01 09:40:46 +0700

    Change name data file

--- cypress/e2e/KID/E2E_timeAssignment.cy.js
+++ cypress/e2e/KID/E2E_timeAssignment.cy.js
@@ -1,5 +1,5 @@
 const functions = require('../../support/function')
-const employeeTimeAdjustment = require('../../fixtures/employeeTimeAdjustment.json')
+const employeeTimeAdjustment = require('../../fixtures/employeeTimeAssignment.json')
 
 describe("E2E 1", ()=> {
 
